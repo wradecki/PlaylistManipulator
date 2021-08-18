46d6b1f92693a62a50d6bc17a3d86d2186003cd4
wradecki <wiktor.radecki@mail.com>
2021-08-18 22:41:31 +0200

    Add: parsing list result notification

--- src/main/kotlin/com/wradecki/Main.kt
+++ src/main/kotlin/com/wradecki/Main.kt
@@ -1,14 +1,49 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.window.Tray
+import androidx.compose.ui.window.rememberTrayState
 import androidx.compose.ui.window.singleWindowApplication
+import com.wradecki.View.MenuRow
+import com.wradecki.model.SingleList
 
-fun main() = singleWindowApplication(title = "Playlist manipulator") {
-    Column {
-        for (i in 1..3)
-            Row {
-                Text("Test fgfgkjhsdkfjglhsdkljfhgksjdhfgkljsh fkjshdf kjgshdf jkgshddkjf ghskdljfgh sdjfgh ksdlfhg kdsjhfg klsjdhfgkljhsdklfj hgsklfdjhglskd jgklsj dfhgklsh dkfghs dkufggsdufgisdyfgiosuynsdnfgksdfngkj sdhkj ghsdkjfhg ksjndfgsdjfh gkjsdhfg kljhsdfg kjsdfkjg hsdkfjg hkfdjhh $i")
-            }
+fun main() = singleWindowApplication(
+    title = "Playlist manipulator",
+    icon = MyAppIcon,
+) {
+    val lists = remember { mutableStateListOf(SingleList("", "", emptyList())) }
+    val trayState = rememberTrayState()
+
+    MaterialTheme {
+        Tray(
+            state = trayState,
+            icon = TrayIcon
+        )
+        MenuRow(lists, trayState)
+
+    }
+}
+
+object MyAppIcon : Painter() {
+    override val intrinsicSize = Size(256f, 256f)
+
+    override fun DrawScope.onDraw() {
+        drawOval(Color.Green, Offset(size.width / 4, 0f), Size(size.width / 2f, size.height))
+        drawOval(Color.Blue, Offset(0f, size.height / 4), Size(size.width, size.height / 2f))
+        drawOval(Color.Red, Offset(size.width / 4, size.height / 4), Size(size.width / 2f, size.height / 2f))
+    }
+}
+
+object TrayIcon : Painter() {
+    override val intrinsicSize = Size(256f, 256f)
+
+    override fun DrawScope.onDraw() {
+        drawOval(Color(0xFFFFA500))
     }
 }
---
+++ src/main/kotlin/com/wradecki/View/MenuRow.kt
@@ -0,0 +1,51 @@
+package com.wradecki.View
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.window.FrameWindowScope
+import androidx.compose.ui.window.MenuBar
+import androidx.compose.ui.window.Notification
+import androidx.compose.ui.window.TrayState
+import com.wradecki.model.SingleList
+import com.wradecki.parsers.ParseFail
+import com.wradecki.parsers.ParseSuccess
+import com.wradecki.parsers.SltvParser
+import java.io.File
+import javax.swing.JFileChooser
+
+@Composable
+fun FrameWindowScope.MenuRow(
+    lists: SnapshotStateList<SingleList>,
+    trayState: TrayState
+) {
+    MenuBar {
+        Menu("File", mnemonic = 'F') {
+            Item("Load list", onClick = {
+                var file: File?
+                JFileChooser().apply {
+                    showOpenDialog(null)
+                    file = selectedFile
+                }
+
+                if (file != null) {
+                    parseList(file, lists, trayState)
+
+                }
+            })
+        }
+    }
+}
+
+private fun parseList(file: File?, lists: SnapshotStateList<SingleList>, trayState: TrayState) {
+    val parser = SltvParser()
+    val result = parser.parse(file!!.path)
+    when (result) {
+        is ParseSuccess -> {
+            lists += result.list
+            trayState.sendNotification(Notification("Success", "List added successfully", Notification.Type.Info))
+        }
+        is ParseFail -> {
+            trayState.sendNotification(Notification("Error", result.error, Notification.Type.Error))
+        }
+    }
+}
