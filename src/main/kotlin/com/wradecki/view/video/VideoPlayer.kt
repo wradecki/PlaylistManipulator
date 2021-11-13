52c148fdd5445b19441f1a2f87358587e303c818
Wiktor Radecki <wiktor.radecki@mail.com>
2021-11-13 01:15:12 +0100

    Add: logging, Add: information on error when playing

--- src/main/kotlin/com/wradecki/view/video/VideoPlayer.kt
+++ src/main/kotlin/com/wradecki/view/video/VideoPlayer.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Notification
+import com.wradecki.view.globalState
 import com.wradecki.view.playerState
 import uk.co.caprica.vlcj.media.Media
 import uk.co.caprica.vlcj.media.MediaEventAdapter
@@ -15,6 +17,7 @@ import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent
 import java.awt.Component
 
 val seekableAdapter = SeekableAdapter()
+val loggingAdapter = LoggingAdapter()
 
 @Composable
 fun VideoPlayer(url: String, modifier: Modifier = Modifier) {
@@ -27,6 +30,7 @@ internal fun VideoPlayerImpl(url: String, modifier: Modifier) {
         val mediaPlayer = playerState.mediaPlayerComponent.mediaPlayer()
         val ok = mediaPlayer.media().play(url)
         mediaPlayer.media().events().addMediaEventListener(seekableAdapter)
+        mediaPlayer.media().events().addMediaEventListener(loggingAdapter)
     }
     return SwingPanel(
         background = Color.Transparent,
@@ -47,7 +51,21 @@ fun Component.mediaPlayer(): MediaPlayer {
 
 class SeekableAdapter : MediaEventAdapter() {
     override fun mediaStateChanged(media: Media?, newState: State?) {
+        println("Seekable adapter: $this with state: $newState")
         if (newState == State.PLAYING && playerState.isSeekable.value != playerState.mediaPlayerComponent.mediaPlayer().status().isSeekable)
             playerState.isSeekable.value = playerState.mediaPlayerComponent.mediaPlayer().status().isSeekable
     }
+}
+
+class LoggingAdapter : MediaEventAdapter() {
+    override fun mediaStateChanged(media: Media?, newState: State?) {
+        if (newState == State.ERROR) {
+            globalState.trayState.sendNotification(Notification(
+                title = "Error",
+                message = "Error on playing media",
+                type = Notification.Type.Error
+            ))
+        }
+    }
+
 }
